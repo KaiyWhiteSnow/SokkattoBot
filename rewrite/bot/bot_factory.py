9b76d8f72273ae71ac459eaed1be389c8a47c043
Kaiy <106684837+KaiyWhiteSnow@users.noreply.github.com>
2024-08-29 15:45:14 +0200

    Fixed map not displaying

--- rewrite/bot/bot_factory.py
+++ rewrite/bot/bot_factory.py
@@ -14,9 +14,11 @@ class BotFunctions():
         time = await s.get_time()
         info = await s.get_info()
         map = await s.get_map(False, False, False)
-        map.save(BytesIO(), format='PNG')
-        image_data = base64.b64encode(BytesIO().getvalue()).decode('utf-8')
+        image_buffer = BytesIO()
+        map.save(image_buffer, format='PNG')
+        image_data = base64.b64encode(image_buffer.getvalue()).decode('utf-8')
         html_code = f'<img src="data:image/png;base64,{image_data}" width="500" height="500">'
+
         safe_html_code = Markup(html_code)
         await s.disconnect()
         return safe_html_code, info.name, info.players, info.max_players, info.size, time
